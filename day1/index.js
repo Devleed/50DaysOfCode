56cb1a6a695c85cd53c14db8a24174227813f9ec
Muhammad Waleed <mujahidwaleed11@gmail.com>
2021-11-01 19:39:20 +0500

    changes: day 1 completed

---
+++ day1/index.js
@@ -0,0 +1,30 @@
+function findMaxConsecutiveOnes(nums) {
+   const { maxOccurence } = nums.reduce((acc, cur) => {
+        if(cur === 1) {
+            return {
+                maxOccurence: acc.occurence + 1 > acc.maxOccurence ? acc.occurence + 1 : acc.maxOccurence,
+                occurence: acc.occurence + 1,
+            }
+        } else {
+            return {
+                maxOccurence: acc.occurence > acc.maxOccurence ? acc.occurence : acc.maxOccurence,
+                occurence: 0,
+            }
+        }
+    }, {
+        maxOccurence: 0,
+        occurence: 0
+    })
+
+    return maxOccurence;
+};
+
+
+const findNumbers = function(nums) {
+    return nums.filter(num => Number(String(num).length) % 2 === 0).length;
+};
+
+const sortedSquares = function(nums) {
+   return nums.map(num => Math.pow(Math.abs(num), 2)).sort((a,b) => a - b)
+};
+
